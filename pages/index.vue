c2e980f5b1ba9dbd301fb765fdd1767238f93fac
Emma Ahlander Klinteberg <eklintis@gmail.com>
2024-11-27 16:11:31 +0100

    deleted tailwind classes

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,34 @@
 <template>
-  <section class="bg-gray-400">
-      <div class="flex items-center justify-center h-screen">
-          <div class="rounded-lg shadow-lg  w-full max-w-md bg-gray-800 border-gray-700">
-              <div class="p-6 text-white">
-                  <h1 class="text-xl font-bold  md:text-2xl py-2 mb-2">
-                      Sign in to your account
-                  </h1>
-                  <div v-if="show_alert" class="text-white text-center font-bold p-4 mb-4" :class="alert_variant">
-                      {{ alert_msg }}
-                  </div>
-                  <form class="space-y-6" @submit.prevent="login">
-                      <div>
-                          <label for="email" class="block mb-2 text-sm font-medium">
-                              Your email</label>
-                          <input type="email" name="email" id="email" v-model="form.email"
-                              class="border text-sm rounded-lg w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
-                              placeholder="name@company.com" required>
-                      </div>
-                      <div>
-                          <label for="password" class="block mb-2 text-sm font-medium">Password</label>
-                          <input type="password" name="password" id="password" placeholder="••••••••"
-                              v-model="form.password"
-                              class="border text-sm rounded-lg w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500"
-                              required>
-                      </div>
-                      <button type="submit"
-                          class="w-full bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                          Sign In</button>
-                      <p class="text-sm font-light text-gray-400">
-                          Don’t have an account yet?
-                          <NuxtLink to="/register" class="font-medium hover:underline text-blue-500">Sign up</NuxtLink>
-                      </p>
-                  </form>
-              </div>
-          </div>
-      </div>
-  </section>
+  
+    <div>
+        <h1>
+            Sign in to your account
+        </h1>
+        <div v-if="show_alert"  :class="alert_variant">
+            {{ alert_msg }}
+        </div>
+        <form @submit.prevent="login">
+            <div>
+                <label for="email" >
+                    Your email</label>
+                <input type="email" name="email" id="email" v-model="form.email"
+                    placeholder="name@company.com" required>
+            </div>
+            <div>
+                <label for="password" class="block mb-2 text-sm font-medium">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••"
+                    v-model="form.password"
+                    required>
+            </div>
+            <button type="submit">
+                Sign In</button>
+            <p>
+                Don't have an account yet?
+                <NuxtLink to="/register" >Sign up</NuxtLink>
+            </p>
+        </form>
+</div>
+          
 </template>
 
 <script setup>
