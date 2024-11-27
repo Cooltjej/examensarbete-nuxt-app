821b5d499515b01fd41f8d4c372d3645936f278f
Emma Ahlander Klinteberg <eklintis@gmail.com>
2024-11-27 16:23:33 +0100

    added a page after successful log in

---
+++ pages/private.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Welcome!</h1>
+        <div>You are signed in with:</div>
+        <div>Email: {{ auth.user.email }}</div>
+        <div>Uid: {{ auth.user.uid }}</div>
+    </div>
+  </template>
+  
+  <script setup>
+  definePageMeta({
+  middleware: ['auth'],
+  })
+  const auth = useAuthStore()
+  </script>
